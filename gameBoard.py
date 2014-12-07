9ee1bb3433c2cef38017ddadbe26c7689b010722
ldayananda <lahiru07@gmail.com>
2014-12-07 00:11:11 -0500

    working on the search functionality

--- gameBoard.py
+++ gameBoard.py
@@ -8,7 +8,7 @@ class Grid :
 	def __init__(self) :
 		self.columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 		self.rows = range(1, 10)
-		self.grid = itertools.product(self.columns, self.rows)
+		self.grid = itertools.product(self.rows, self.columns)
 		self.gridValues = {}
 
 	def initialize(self) :
@@ -18,18 +18,6 @@ class Grid :
 		domain = range(1, 10)
 		for cell in self.grid :
 			values[cell] = list(domain)
-
-		self.gridValues = values
-		# print self.gridValues
-		return self.gridValues
-
-	def placeHints(self, hints) :
-		values = {}
-		for cell in self.gridValues :
-			if hints.__contains__(cell) :
-				values[cell] = hints[cell]
-			else :
-				values[cell] = list(range(1, 10))
 
 		self.gridValues = values
 		# print self.gridValues
@@ -63,22 +51,28 @@ class Grid :
 
 		return False
 
-	def getBox(self, row, column) :
-		box1 = list(itertools.product(['A', 'B', 'C'], [1, 2, 3]))
-		box2 = list(itertools.product(['D', 'E', 'F'], [1, 2, 3]))
-		box3 = list(itertools.product(['G', 'H', 'I'], [1, 2, 3]))
+	def getBoxes(self) :
+		box1 = list(itertools.product([1, 2, 3], ['A', 'B', 'C']))
+		box2 = list(itertools.product([1, 2, 3], ['D', 'E', 'F']))
+		box3 = list(itertools.product([1, 2, 3], ['G', 'H', 'I']))
 
-		box4 = list(itertools.product(['A', 'B', 'C'], [4, 5, 6]))
-		box5 = list(itertools.product(['D', 'E', 'F'], [4, 5, 6]))
-		box6 = list(itertools.product(['G', 'H', 'I'], [4, 5, 6]))
+		box4 = list(itertools.product([4, 5, 6], ['A', 'B', 'C']))
+		box5 = list(itertools.product([4, 5, 6], ['D', 'E', 'F']))
+		box6 = list(itertools.product([4, 5, 6], ['G', 'H', 'I']))
 
-		box7 = list(itertools.product(['A', 'B', 'C'], [7, 8, 9]))
-		box8 = list(itertools.product(['D', 'E', 'F'], [7, 8, 9]))
-		box9 = list(itertools.product(['G', 'H', 'I'], [7, 8, 9]))
+		box7 = list(itertools.product([7, 8, 9], ['A', 'B', 'C']))
+		box8 = list(itertools.product([7, 8, 9], ['D', 'E', 'F']))
+		box9 = list(itertools.product([7, 8, 9], ['G', 'H', 'I']))
 
-		cell = (row, column)
 		boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
 
+		return boxes
+
+	def getBox(self, row, column, boxes=None) :
+		if boxes == None :
+			boxes = self.getBoxes()
+
+		cell = (row, column)
 		for box in boxes :
 			if box.__contains__(cell) :
 				return box
@@ -119,7 +113,7 @@ class Grid :
 
 	def setValue(self, row, column, digit) :
 		cell = (row, column)
-		self.gridValues[cell] = digit
+		self.gridValues[cell] = [digit]
 
 		return self.gridValues
 
@@ -129,6 +123,80 @@ class Grid :
 		# print self.gridValues
 		return self.gridValues
 
+	def placeHints(self, hints) :
+		values = {}
+		for cell in self.gridValues :
+			if hints.__contains__(cell) :
+				values[cell] = [hints[cell]]
+			else :
+				values[cell] = list(range(1, 10))
+
+		self.gridValues = values
+
+		# remove the hints from threatened domains
+		for cell in hints :
+			self.removeDigit(cell[0], cell[1], hints[cell])
+
+		# print self.gridValues
+		return self.gridValues
+
+	def parseGrid(self, string) :
+		chars = [c for c in string if (c in '.0123456789')]
+		values = {}
+		i = 0
+		cell = self.grid.next()
+		try :
+			for char in chars :
+				if not char == '.' :
+					self.gridValues[cell] = char
+				else :
+					self.gridValues[cell] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+				cell = self.grid.next()
+		except StopIteration :
+			pass
+		finally :
+			return self.gridValues
+
+
+	def printGrid(self) :
+		domains = self.gridValues
+		print "------------------------------------------------------------------------------------------"
+
+		row1 = self.getRow(1)
+		row1.sort(key=lambda x: x[1])
+		row2 = self.getRow(2)
+		row2.sort(key=lambda x: x[1])
+		row3 = self.getRow(3)
+		row3.sort(key=lambda x: x[1])
+		row4 = self.getRow(4)
+		row4.sort(key=lambda x: x[1])
+		row5 = self.getRow(5)
+		row5.sort(key=lambda x: x[1])
+		row6 = self.getRow(6)
+		row6.sort(key=lambda x: x[1])
+		row7 = self.getRow(7)
+		row7.sort(key=lambda x: x[1])
+		row8 = self.getRow(8)
+		row8.sort(key=lambda x: x[1])
+		row9 = self.getRow(9)
+		row9.sort(key=lambda x: x[1])
+
+		rows = [row1, row2, row3, row4, row5, row6, row7, row8, row9]
+
+		width = 1+max(len(domains[rows[i][j]]) for i in range(len(rows)) for j in range(len(rows[i])))
+		line = ''.join([ '-' * (width * 3)] * 3)
+
+		for row in rows:
+			temp = ""
+			for i in range(len(row)) :
+				values = domains[row[i]] if len(domains[row[i]]) == 1 else '.'
+				temp += str(values).center(width) + (' | ' if i == 2 or i == 5 else ' ') 
+			print temp
+			if rows.index(row) == 2 or rows.index(row) == 5 : 
+				print line
+
+			print 
 
 class EasyGrid(Grid) :
 
@@ -146,3 +214,4 @@ class DifficultGrid(Grid) :
 	def initialize(self, hints) :
 		# 3 pre-solved cells
 		self.placeHints(hints)
+		
--- search.py
+++ search.py
@@ -1,2 +1,35 @@
 # place a value and remove the from domains of constrained areas
 # search algorithm will return what cells and values are picked
+
+from gameBoard import Grid
+
+class SearchAgent :
+
+	def __init__(self, grid) :
+		self.grid = grid
+		self.foundValues = dict((cell, False) for cell in self.grid.gridValues)
+
+	def search(self) :
+		# find a value that can be assigned and assign it
+		values = self.grid.gridValues
+
+		# while not all(value == True for value in self.foundValues) :
+		## TODO : figure out whether the remove is working correctly for all instances
+		for cell in values :
+			if len(values[cell]) == 1 :
+				# will trigger removing from the domains of all other cells threatened
+				self.grid.setValueAndRemoveDigit(cell[0], cell[1], values[cell][0])
+				self.foundValues[cell] = True
+				print self.foundValues
+			elif len(values[cell]) == 0 :
+				print "no"
+
+		# finally
+		return self.grid.gridValues
+
+two = "..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."
+agrid = Grid()
+agrid.parseGrid(two)
+agent = SearchAgent(agrid)
+agent.search()
+agent.grid.printGrid()
